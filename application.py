d5dcd4b7f39b07d9086269027517c5ef5edb4057
Jason Ling <jtg2078@gmail.com>
2015-07-09 17:26:40 +0800

    completed CRUD for category and subcategory

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from functools import wraps
 
 # flask related imports
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, g
 from flask import session as login_session
 
 # database related imports
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Subcategory, Item
 
@@ -31,11 +31,56 @@ def login_required(f):
 
     return wrap
 
+# category required decorator
+def category_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        category_name = kwargs.get('category_name', '')
+        category = (db_session.query(Category)
+                    .filter(Category.name == category_name)
+                    .first())
+        if not category:
+            flash('category with name {0} does not exist'.format(category_name))
+            return redirect(url_for('home'))
+        else:
+            kwargs['category'] = category
+            return f(*args, **kwargs)
+
+    return wrap
+
+# subcategory required decorator
+def subcategory_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        category = kwargs.get('category', None)
+        subcategory_name = kwargs.get('subcategory_name', '')
+        subcategory = (db_session.query(Subcategory)
+                                 .filter(Subcategory.name == subcategory_name)
+                                 .filter(Subcategory.category_id == category.id)
+                                 .first())
+        if not subcategory:
+            flash('subcategory with name {0} does not exist'.format(subcategory_name))
+            return redirect(url_for('home'))
+        else:
+            kwargs['subcategory'] = subcategory
+            return f(*args, **kwargs)
+
+    return wrap
+
+#  ------------------------------  index ------------------------------
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", user=login_session.get('user'))
+    items = db_session.query(Item).order_by(desc(Item.added))
+    catalog = db_session.query(Category).order_by(asc(Category.id))
+    return render_template("index.html",
+                           user=login_session.get('user'),
+                           catalog=catalog,
+                           latest_items=items)
 
+
+#  ------------------------------  login / logout ------------------------------
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -58,24 +103,136 @@ def logout():
     return redirect(url_for('home'))
 
 
+#  ------------------------------  category ------------------------------
+
+
 @app.route("/catalog/<category_name>/items")
-def category(category_name=None):
-    return "this is the {0}'s items listing page".format(category_name)
+@category_required
+def show_category(category_name=None, category=None):
+    catalog = db_session.query(Category).order_by(asc(Category.id))
+    subcategories = (db_session.query(Subcategory)
+                               .filter(Subcategory.category_id == category.id)
+                               .order_by(asc(Subcategory.id)))
+    items = (db_session.query(Item)
+                       .filter(Item.category_id == category.id)
+                       .order_by(asc(Item.id)))
+    return render_template('category.html',
+                           user=login_session.get('user'),
+                           catalog=catalog,
+                           category=category,
+                           subcategories=subcategories,
+                           items=items)
 
 
-@app.route('/catalog/category/new', methods=['POST'])
+@app.route('/catalog/category/new', methods=['POST', 'GET'])
 @login_required
 def new_category():
-    new_category = Category(name=request.form['name'])
-    db_session.add(new_category)
+    error = None
+    if request.method == 'POST':
+        category_name = request.form.get('name', '').strip()
+        if category_name:
+            category = Category(name=category_name)
+            db_session.add(category)
+            db_session.commit()
+            flash('New category %s Successfully Created' % category.name)
+            return redirect(url_for('home'))
+        else:
+            error = 'category name is missing'
+    return render_template('category_new.html', error=error, user=login_session.get('user'))
+
+
+@app.route("/catalog/<category_name>/edit", methods=['POST', 'GET'])
+@login_required
+@category_required
+def edit_category(category_name=None, category=None):
+    error = None
+    if request.method == 'POST':
+        category_name = request.form.get('name', '').strip()
+        if category_name:
+            category.name = category_name
+            db_session.add(category)
+            db_session.commit()
+            flash('Category updated!')
+            return redirect(url_for('show_category', category_name=category_name))
+        else:
+            error = 'category name is missing'
+    return render_template('category_edit.html', category=category, user=login_session.get('user'), error=error)
+
+
+@app.route("/catalog/<category_name>/delete", methods=['POST'])
+@login_required
+@category_required
+def delete_category(category_name=None, category=None):
+    db_session.delete(category)
     db_session.commit()
-    flash('New Category %s Successfully Created' % new_category.name)
+    flash('Category {0} deleted!'.format(category_name))
     return redirect(url_for('home'))
 
 
+#  ------------------------------  subcategory ------------------------------
+
+@app.route('/catalog/<category_name>/subcategory/new', methods=['POST', 'GET'])
+@login_required
+@category_required
+def new_subcategory(category_name, category):
+    error = None
+    if request.method == 'POST':
+        subcategory_name = request.form.get('name', '').strip()
+        if subcategory_name:
+            subcategory = Subcategory(name=subcategory_name,
+                                      category_id=category.id)
+            db_session.add(subcategory)
+            db_session.commit()
+            flash('New subcategory %s created successfully' % subcategory.name)
+            return redirect(url_for('show_category', category_name=category_name))
+        else:
+            error = 'subcategory name is missing'
+    return render_template('subcategory_new.html', category=category, error=error, user=login_session.get('user'))
+
+
+@app.route("/catalog/<category_name>/subcategory/<subcategory_name>/edit", methods=['POST', 'GET'])
+@login_required
+@category_required
+@subcategory_required
+def edit_subcategory(category_name=None, category=None, subcategory_name=None, subcategory=None):
+    error = None
+    if request.method == 'POST':
+        subcategory_name = request.form.get('name', '').strip()
+        if subcategory_name:
+            subcategory.name = subcategory_name
+            db_session.add(subcategory)
+            db_session.commit()
+            flash('Subcategory updated!')
+            return redirect(url_for('show_category', category_name=category_name))
+        else:
+            error = 'Subcategory name is missing'
+    return render_template('subcategory_edit.html',
+                           category=category,
+                           subcategory=subcategory, user=login_session.get('user'), error=error)
+
+
+@app.route("/catalog/<category_name>/subcategory/<subcategory_name>/delete", methods=['POST'])
+@login_required
+@category_required
+@subcategory_required
+def delete_subcategory(category_name=None, category=None, subcategory_name=None, subcategory=None):
+    db_session.delete(subcategory)
+    db_session.commit()
+    flash('Subcategory {0} deleted!'.format(subcategory_name))
+    return redirect(url_for('show_category', category_name=category_name))
+
+
+#  ------------------------------  item ------------------------------
+
+
 @app.route("/catalog/<category_name>/<item_name>")
-def item(category_name=None, item_name=None):
+def show_item(category_name=None, item_name=None):
     return "this is item {0}'s page".format(item_name)
+
+
+@app.route("/catalog/item/new")
+def new_item():
+    return "this is add new item page"
 
 
 @app.route("/catalog/<item_name>/edit")
--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -67,6 +67,8 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String)
+    added = Column(DateTime)
+    updated  = Column(DateTime)
     subcategory_id = Column(Integer, ForeignKey('subcategory.id'))
     subcategory = relationship(Subcategory)
     category_id = Column(Integer, ForeignKey('category.id'))
